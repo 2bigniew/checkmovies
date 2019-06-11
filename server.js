a89931f70733a85dd5076b07e4df0cfc4ca65a26
2bigniew <stasiakzb@gmail.com>
2019-06-11 18:42:46 +0200

    add chat functionality

--- server.js
+++ server.js
@@ -6,4 +6,13 @@ const server = app.listen(app.get('port'), () => {
     console.log(`Listening on ${server.address().port}`);
 });
 
+// Chat **************************************************************
+const io = require('socket.io')(server);
+app.set('socket_io_movie_chat', io);
+
+io.on('connection', function(socket) {
+    socket.on('chat-start', (data) => io.sockets.emit('chat-start', data));
+    socket.on('typing', (data) => socket.broadcast.emit('typing', data));
+});
+
 module.exports = server;
